955e5c570d5fa6c2406d306b54f2f9224ae73896
Yann Lorthioir <y_lorthioir@msn.com>
2024-03-19 11:16:09 +0100

    Dependancies update

--- src/main/java/be/ylorthioir/dal/ConnectionFactory.java
+++ src/main/java/be/ylorthioir/dal/ConnectionFactory.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public abstract class ConnectionFactory {
 	public static final String URL= "jdbc:postgresql://localhost:5432/jdbc";
-	public static final String USERNAME= "Yann";
-	public static final String PASSWORD = "Charlotte2901";
+	public static final String USERNAME= "postgres";
+	public static final String PASSWORD = "password";
 
 	public static Connection createConnection() throws SQLException {
 		return DriverManager.getConnection(URL,USERNAME,PASSWORD);
